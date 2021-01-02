2c64fd662694629902e67876c1ff24fae79fa8ee
Stefan Dej <meteyou@gmail.com>
2021-01-02 14:35:57 +0100

    test upload

---
+++ precache-manifest.bb7994fdeeec08c0813a549506a1ffa4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55743877f3ffd5fc834e97bc43a6e7bd",
+    "url": "/config.json"
+  },
+  {
+    "revision": "21a2f0ff94fec32f9dad",
+    "url": "/css/app.9f1cf960.css"
+  },
+  {
+    "revision": "d118b2685a217f9eaa26",
+    "url": "/css/chunk-vendors.6c664d1e.css"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "/fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "/fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "/fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "/fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "3abd8a68dc5c20d00d1ab8def9723404",
+    "url": "/fonts/roboto-black.woff2"
+  },
+  {
+    "revision": "a59072f933169d3f2db497f44ca4cbbe",
+    "url": "/fonts/roboto-bold.woff2"
+  },
+  {
+    "revision": "5591b62beff3a20beaedd6cace4c6520",
+    "url": "/fonts/roboto-light.woff2"
+  },
+  {
+    "revision": "07db243db21ed0a6b4ff05ff429686b7",
+    "url": "/fonts/roboto-medium.woff2"
+  },
+  {
+    "revision": "15fa3062f8929bd3b05fdca5259db412",
+    "url": "/fonts/roboto-regular.woff2"
+  },
+  {
+    "revision": "d0151dbc1398a43954bb2b089a80826f",
+    "url": "/fonts/roboto-thin.woff2"
+  },
+  {
+    "revision": "5b493fc130bcd437e8312ee77e91e267",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "774a2eaa602d5066f03e868e16e2b08e",
+    "url": "/img/sidebar-background.png"
+  },
+  {
+    "revision": "71838e4d8515a557eb79d0bfc2cf977b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a2f0ff94fec32f9dad",
+    "url": "/js/app.dc4baa75.js"
+  },
+  {
+    "revision": "d118b2685a217f9eaa26",
+    "url": "/js/chunk-vendors.018af67b.js"
+  },
+  {
+    "revision": "cccb84463c222de77b76c3f3e2f8cf86",
+    "url": "/manifest.json"
+  }
+]);
